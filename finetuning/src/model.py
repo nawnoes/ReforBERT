3889471533d9b96d262e77a12f7e8025eaa3ca97
nawnoes <nawnoes@gmail.com>
2020-05-31 01:46:22 +0900

    start to develop code for squad fine-tuning

--- finetuning/src/model.py
+++ finetuning/src/model.py
@@ -2,9 +2,26 @@ import torch.nn as nn
 from torch.nn import CrossEntropyLoss, MSELoss
 from reformer.model import ReforBertLM, Reformer
 
-class ReforBertForQA(nn.Module):
+class ReforBertForQA(nn.module):
     def __init__(self, config):
         super(ReforBertForQA, self)
 
+        vocab_size = 8007     # vocab 크기
+        max_seq_len = 512     # 최대 입력 길이
+        embedding_size = 768  # 임베딩 사이
+        batch_size = 128      # 학습 시 배치 크기
+        depth = 6             # reformer depth
+        heads = 8             # reformer heads
+
         self.num_labels = config.num_labels
-        self.reforBert = ReforBertLM()
+        self.reforBert = ReforBertLM(
+                            num_tokens=vocab_size,
+                            dim=embedding_size,
+                            depth=depth,
+                            heads=heads,
+                            max_seq_len=max_seq_len,
+                            causal=True
+                        )
+        self.qa_output = nn.Linear(embedding_size, self.num_labels)
+
+        torch.nn.init.xavier_uniform_(self.qa_output.weight)
