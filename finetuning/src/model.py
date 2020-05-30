6adae6e5671f056bddccf2fab6ac21f3c14a5cdb
nawnoes <nawnoes@gmail.com>
2020-05-30 21:33:42 +0900

    start to develop code for squad fine-tuning

--- finetuning/src/model.py
+++ finetuning/src/model.py
@@ -2,4 +2,9 @@ import torch.nn as nn
 from torch.nn import CrossEntropyLoss, MSELoss
 from reformer.model import ReforBertLM, Reformer
 
-class ReforbertForSequenceClassification(ReforBertLM)
+class ReforBertForQA(nn.Module):
+    def __init__(self, config):
+        super(ReforBertForQA, self)
+
+        self.num_labels = config.num_labels
+        self.reforBert = ReforBertLM()
